da3de9d98f887df32ceccb1358891ed1d18b3348
Melina Fuch <sosafuchmelina@gmail.com>
2023-11-30 21:41:32 -0300

    add components for routing

---
+++ src/app/modules/clients/clients.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-clients',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './clients.component.html',
+  styleUrl: './clients.component.scss'
+})
+export class ClientsComponent {
+
+}
---
+++ src/app/modules/sales/sales.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sales',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sales.component.html',
+  styleUrl: './sales.component.scss'
+})
+export class SalesComponent {
+
+}

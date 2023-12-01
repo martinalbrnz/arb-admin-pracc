f39c98ee796bdcaf1e4f14c10995ac576a16356c
camila <camilamaita07@gmail.com>
2023-12-01 20:28:09 -0300

    routing and imports components, in app.components/app.routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { AuthComponent } from '@modules/auth/auth.component'
+import { ClientsComponent } from '@modules/clients/clients.component'
+import { LoginComponent } from '@modules/login/login.component'
+import { ProductsComponent } from '@modules/products/products.component'
+import { SalesComponent } from '@modules/sales/sales.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoginComponent, AuthComponent, ClientsComponent, ProductsComponent, SalesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router'
+import { AuthComponent } from '@modules/auth/auth.component'
+import { ClientsComponent } from '@modules/clients/clients.component'
+import { LoginComponent } from '@modules/login/login.component'
+import { ProductsComponent } from '@modules/products/products.component'
+import { SalesComponent } from '@modules/sales/sales.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+    {
+        path: "", component: AuthComponent,
+        children: [
+            {
+                path: "clients",
+                component: ClientsComponent
+            },
+            {
+                path: "products",
+                component: ProductsComponent
+            },
+            {
+                path: "sales",
+                component: SalesComponent
+            }
+        ]
+    },
+
+    {
+        path: "login", component: LoginComponent
+    }
+
+]

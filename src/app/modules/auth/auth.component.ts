9b80b0eac4f4a12468166c2e2728506fe52a5629
Lazaro Sanchez <lazarosanchezjs@gmail.com>
2023-12-01 21:25:06 -0300

    add: tailwind css + config (#2)
    * add: tailwind css + config
    
    * routing and imports components, in app.components/app.routes
    
    ---------
    
    Co-authored-by: camila <camilamaita07@gmail.com>

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })

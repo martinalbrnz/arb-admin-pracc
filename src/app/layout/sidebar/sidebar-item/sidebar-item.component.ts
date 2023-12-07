6a7cc0b2b100dc395b652f22165ccfc5499bb9e6
camila <camilamaita07@gmail.com>
2023-12-07 00:13:21 -0300

    created sidebar-item to sidebar

---
+++ src/app/layout/sidebar/sidebar-item/sidebar-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidebar-item.component.html',
+  styleUrl: './sidebar-item.component.scss'
+})
+export class SidebarItemComponent {
+
+}
